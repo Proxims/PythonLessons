ac7e0e3014a4ea5533702b24113ce0e5d43f90a3
Proxim <Proxim@list.ru>
2021-02-08 18:14:34 +0300

    Lessons renamed with underscores "_"

--- Lesson_16_Modules.py
+++ Lesson_16_Modules.py
@@ -1,7 +1,7 @@
 # Lesson-16-Modules
 import Lesson_16_MOD            # Импорт модуля
 
-from Lesson_16_MOD import *     # Импорт функций из м
+from Lesson_16_MOD import *     # Импорт функций из модуля
 
 # -----------------------------MAIN-------------------------
 Lesson_16_MOD.aaa()             # Вывод функций указвая модуль
